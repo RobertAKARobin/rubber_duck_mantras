222914e876142ad5b7ef6b8b436149ac383faea7
RobertAKARobin <robertgfthomas@gmail.com>
2016-01-27 16:54:01 -0500

    Added sweet index page

--- rubberduck.js
+++ rubberduck.js
@@ -1,9 +1,19 @@
 var app = require("express")();
+var fs = require("fs");
 var mantras;
+var index;
 
-require("fs").readFile("mantras.html", "utf8", function(err, data){
+fs.readFile("mantras.html", "utf8", function(err, data){
   mantras = data.trim().split("\n");
 });
+
+fs.readFile("index.html", "utf8", function(err, data){
+  index = data;
+});
+
+function sample(){
+  return mantras[Math.floor(Math.random() * (mantras.length))];
+}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,11 +22,15 @@ app.use(function(req, res, next) {
 });
 
 app.get("/", function(req, res){
+  res.send(index.replace("{{mantra}}", sample()));
+});
+
+app.get("/all", function(req, res){
   res.json(mantras);
 });
 
 app.get("/one", function(req, res){
-  res.json({mantra: mantras[Math.floor(Math.random() * (mantras.length))]});
+  res.json({mantra: sample()});
 });
 
 app.listen(3003);
